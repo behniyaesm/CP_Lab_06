37f724eb81e8f5baec38df712f66c754c8e132f2
behniyaesm <behniyaesm@gmail.com>
2024-10-03 17:25:46 -0400

    Completed task 2

--- src/FuelCost.java
+++ src/FuelCost.java
@@ -16,32 +16,49 @@ public class FuelCost
         boolean done = false;
 
         do {
-            System.out.println("Enter gallons of gas your tank can hold: ");
+            System.out.println("Enter how many gallons of gas your tank can hold: ");
             if (sc.hasNextDouble()) {
                 gallonsInTank = sc.nextDouble();
                 done = true;
             } else {
                 trash = sc.next();
-                System.out.println("\nYou said your car can hold " + trash + " gallons of gas");
+                System.out.println("\nYou said your car can hold " + trash + " gallons of gas.");
                 System.out.println("You have to enter a valid amount!");
             }
         } while (!done);
 
+        done = false; //reset boolean for next input
         do {
+            System.out.println("Enter your cars fuel efficiency in mpg: ");
+            if (sc.hasNextDouble()) {
+                milesPerGallon = sc.nextDouble();
+                done = true;
+            } else {
+                trash = sc.next();
+                System.out.println("\nYou said your cars fuel efficiency was " + trash + " mpg.");
+                System.out.println("You have to enter a valid mpg!");
+            }
+        } while (!done);
 
-            System.out.println("Enter Miles per gallon for your car: ");
-            milesPerGallon = sc.nextDouble();
-
-            System.out.println("Enter the price of gas per gallon: ");
-            pricePerGallon = sc.nextDouble();
-        } while (pricePerGallon >= 0);
+        done = false; //reset boolean for next input
+        do {
+            System.out.println("Enter the price of the gas per gallon: ");
+            if (sc.hasNextDouble()) {
+                pricePerGallon = sc.nextDouble();
+                done = true;
+            } else {
+                trash = sc.next();
+                System.out.println("\nYou said the price of gas was " + trash);
+                System.out.println("You have to enter a valid price!");
+            }
+        } while (!done);
 
         fuelNeeded = milesDriven / milesPerGallon;
         fuelCost = fuelNeeded * pricePerGallon;
 
-        System.out.println("The cost to drive 100 miles is: " + fuelCost);
+        System.out.println("The cost to drive 100 miles is $" + fuelCost);
 
         distance = gallonsInTank * milesPerGallon;
-        System.out.println("Your car can go " + distance + " miles per gallon");
+        System.out.println("Your car can go " + distance + " miles with a full tank of gas.");
     }
 }
